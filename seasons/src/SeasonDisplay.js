ff13140a282dd85ef2daa9b40492a4c121c688f2
jethrofloyd <ritobrotomaitra@gmail.com>
2018-12-26 01:27:49 +0530

    Built Conditional Rendering with Lifecycles

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,7 +1,34 @@
+import "./SeasonDisplay.css";
 import React from "react";
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>;
+const seasonConfig = {
+  summer: {
+    textToDisplay: "Hot!",
+    iconName: "sun"
+  },
+  winter: {
+    textToDisplay: "Cold!",
+    iconName: "snowflake"
+  }
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { iconName, textToDisplay } = seasonConfig[season];
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
+      <h1>{textToDisplay}</h1>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,29 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: 40, long: 50 };
+    this.state = { lat: null, long: null, errorMessage: "" };
+  }
+
+  componentDidMount() {
+    console.log("The component rendered");
     window.navigator.geolocation.getCurrentPosition(
       position => {
+        console.log("Received geolocation");
         this.setState({
           lat: position.coords.latitude,
           long: position.coords.longitude
         });
+        console.log("Set geolocation");
       },
-      err => console.log(err)
+      err => {
+        console.log(err.message);
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
+  componentDidUpdate() {
+    console.log("The component updated");
+  }
+  // Helper function for rendering conditional content.
+  renderContent() {
+    if (!this.state.lat && !this.state.errorMessage) return <Spinner />;
+    if (!this.state.lat && this.state.errorMessage)
+      return <div>Error: {this.state.errorMessage}</div>;
+    return <SeasonDisplay lat={this.state.lat} />;
+  }
+
   render() {
-    return (
-      <div>
-        <div>Latitude: {this.state.lat}</div>
-        <div>Longitude: {this.state.long}</div>
-      </div>
-    );
+    console.log("Rendering Done!");
+    return <div>{this.renderContent()}</div>;
   }
 }
 
