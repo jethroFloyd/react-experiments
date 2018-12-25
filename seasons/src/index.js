57212f65970615c67795ae90619a0514caadb48d
jethrofloyd <ritobrotomaitra@gmail.com>
2018-12-25 21:30:10 +0530

    Moved to Classes from Functions

---
+++ seasons/src/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: 40, long: 50 };
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          lat: position.coords.latitude,
+          long: position.coords.longitude
+        });
+      },
+      err => console.log(err)
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Latitude: {this.state.lat}</div>
+        <div>Longitude: {this.state.long}</div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
