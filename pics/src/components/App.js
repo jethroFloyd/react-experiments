245f6f3ec04f1398a44b4fec50b6df0db7c3672b
jethrofloyd <ritobrotomaitra@gmail.com>
2019-01-01 22:02:23 +0530

    Using Promise with Axios

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,29 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  onSearchSubmit(term) {
+    axios
+      .get("https://api.unsplash.com/search/photos", {
+        params: { query: term },
+        headers: {
+          Authorization:
+            "Client-ID 83171c7f594c24ef8c3923abd6c2754e4c910e37a48048fe8d85e352a138cb67"
+        }
+      })
+      .then(response => {
+        console.log(response.data.results);
+      });
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
