31cd9c16fd26ed1c89c1bb6bd601a4201816ec41
jethrofloyd <ritobrotomaitra@gmail.com>
2019-01-11 17:09:34 +0530

    Experiments with BPJS

--- emaily/server/client/src/components/App.js
+++ emaily/server/client/src/components/App.js
@@ -1,17 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./Header";
-import { Button } from "@blueprintjs/core";
 
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => (
-  <Button rightIcon="arrow-right" intent="success" text="Next step" />
-);
+const Landing = () => <p style={{ margin: "10px" }}>Some Text</p>;
 
 const App = () => {
   return (
-    <div>
+    <div style={{ margin: "0 auto" }}>
       <BrowserRouter>
         <div>
           <Header />
--- emaily/server/client/src/components/Header.js
+++ emaily/server/client/src/components/Header.js
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
+import {
+  Alignment,
+  Button,
+  Classes,
+  H5,
+  Navbar,
+  NavbarDivider,
+  NavbarGroup,
+  NavbarHeading,
+  Switch
+} from "@blueprintjs/core";
+
+import { Icon, Intent } from "@blueprintjs/core";
+import { IconNames } from "@blueprintjs/icons";
+import { IconName } from "@blueprintjs/icons";
 
 class Header extends React.Component {
   render() {
-    return <div> Header </div>;
+    return (
+      <Navbar className="bp3-dark">
+        <Navbar.Group align={Alignment.LEFT}>
+          <Navbar.Heading>ChironEye</Navbar.Heading>
+          <Navbar.Divider />
+        </Navbar.Group>
+        <Navbar.Group align={Alignment.RIGHT}>
+          <Button intent="primary" icon="log-in" text="Sign In With Google" />
+        </Navbar.Group>
+      </Navbar>
+    );
   }
 }
 
