75137ac3617ffefa721f490a4e5a38686a575073
jethrofloyd <ritobrotomaitra@gmail.com>
2019-01-11 17:40:09 +0530

    Setup Redux Thunk and Axios

--- emaily/server/client/src/components/Header.js
+++ emaily/server/client/src/components/Header.js
@@ -1,19 +1,5 @@
 import React, { Component } from "react";
-import {
-  Alignment,
-  Button,
-  Classes,
-  H5,
-  Navbar,
-  NavbarDivider,
-  NavbarGroup,
-  NavbarHeading,
-  Switch
-} from "@blueprintjs/core";
-
-import { Icon, Intent } from "@blueprintjs/core";
-import { IconNames } from "@blueprintjs/icons";
-import { IconName } from "@blueprintjs/icons";
+import { Alignment, Button, Navbar } from "@blueprintjs/core";
 
 class Header extends React.Component {
   render() {
@@ -21,7 +7,6 @@ class Header extends React.Component {
       <Navbar className="bp3-dark">
         <Navbar.Group align={Alignment.LEFT}>
           <Navbar.Heading>ChironEye</Navbar.Heading>
-          <Navbar.Divider />
         </Navbar.Group>
         <Navbar.Group align={Alignment.RIGHT}>
           <Button intent="primary" icon="log-in" text="Sign In With Google" />
