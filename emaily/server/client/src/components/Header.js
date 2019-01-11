a45705ab0989bdaaa49dd2805aa467c9658f43d3
jethrofloyd <ritobrotomaitra@gmail.com>
2019-01-12 00:37:16 +0530

    Access State in Header

--- emaily/server/client/src/components/Header.js
+++ emaily/server/client/src/components/Header.js
@@ -1,19 +1,38 @@
 import React from "react";
-import { Alignment, Button, Navbar } from "@blueprintjs/core";
+import { connect } from "react-redux";
+import { Alignment, Button, Navbar, Spinner } from "@blueprintjs/core";
 
 class Header extends React.Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return <Spinner intent="primary" size="20" value={null} />;
+      case false:
+        return (
+          <Button intent="primary" icon="log-in" text="Sign In With Google" />
+        );
+      default:
+        return <p>Logged In!</p>;
+    }
+  }
+
   render() {
+    console.log(this.props.auth);
     return (
       <Navbar className="bp3-dark">
         <Navbar.Group align={Alignment.LEFT}>
           <Navbar.Heading>ChironEye</Navbar.Heading>
         </Navbar.Group>
         <Navbar.Group align={Alignment.RIGHT}>
-          <Button intent="primary" icon="log-in" text="Sign In With Google" />
+          {this.renderContent()}
         </Navbar.Group>
       </Navbar>
     );
   }
 }
 
-export default Header;
+function mapStateToProps(auth) {
+  return auth;
+}
+
+export default connect(mapStateToProps)(Header);
