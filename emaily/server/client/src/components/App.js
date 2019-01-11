d14b40d5f184466b65e201a3f9dc4c918946d911
jethrofloyd <ritobrotomaitra@gmail.com>
2019-01-11 16:20:21 +0530

    Add BluePrintJS

--- emaily/server/client/src/components/App.js
+++ emaily/server/client/src/components/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
+import { Button } from "@blueprintjs/core";
 
-const Header = () => <h2>Header</h2>;
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
+const Landing = () => (
+  <Button rightIcon="arrow-right" intent="success" text="Next step" />
+);
 
 const App = () => {
   return (
---
+++ emaily/server/client/src/components/Header.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Header extends React.Component {
+  render() {
+    return <div> Header </div>;
+  }
+}
+
+export default Header;
